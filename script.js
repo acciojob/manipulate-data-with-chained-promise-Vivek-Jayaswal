21c7a7edeb705ce359e0700f6f6042ffd9d703d8
Vivek-Jayaswal <141914455+Vivek-Jayaswal@users.noreply.github.com>
2024-05-03 15:50:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 //your JS code here. If required.
+//your JS code here. If required.
+const output = document.getElementById("output");
+function getData (){
+	return new Promise((resolve) => {
+		setTimeout(()=>{
+			resolve([1, 2, 3, 4])
+		},3000)
+	})
+}
+
+getData()
+	.then((data) => {
+		return new Promise((resolve) => {
+			setTimeout(()=>{
+				resolve(data.filter((ele)=> ele%2 === 0))
+			},1000)
+		})
+	})
+	.then((data) => {
+		return new Promise((resolve)=> {
+			 setTimeout(() => {
+				 resolve(data.map((multipliedNumber) => multipliedNumber*2))
+			 },2000)
+		})
+		
+	})
+	.then((result) => {
+		const output = document.getElementById("output");
+		output.innerText = result.join(" ");
+	})
